df0353b431da832af007918b3e3848b40ce13914
E B <evgbay8@gmail.com>
2025-01-21 13:20:19 +0500

    initial. memcached working

---
+++ books/forms.py
@@ -0,0 +1,19 @@
+from django.forms import ModelForm, CharField, TextInput
+from .models import Book
+from django.utils.translation import gettext_lazy as _
+
+
+class BookCreateForm(ModelForm):
+    title = CharField(required=False, widget=TextInput(attrs={"class": "clrtxt", "placeholder": _("Title")}))
+    author = CharField(required=False, widget=TextInput(attrs={"class": "clrtxt", "placeholder": _("Author")}))
+    price = CharField(required=False, widget=TextInput(attrs={"class": "clrtxt", "placeholder": _("Price")}))
+
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'price']
+
+
+class BookEditForm(BookCreateForm):
+    title  = CharField(required=False, widget=TextInput(attrs={"class":"form-control-sm form-control"}))
+    author = CharField(required=False, widget=TextInput(attrs={"class":"form-control-sm form-control"}))
+    price  = CharField(required=False, widget=TextInput(attrs={"class":"form-control-sm form-control"}))
